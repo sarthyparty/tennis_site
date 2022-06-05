d2a8236849aa278d8378d07217694b4d7671d69d
Sarthak Agrawal <agraw189@umn.edu>
2022-06-04 22:08:29 -0500

    stie

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className="Home">
+      <article>
+          <h1>Home</h1>
+          This is the home page will have picture of team and quick description
+        </article>
+    </div>
+  );
+}
+
+export default Home;
+
---
+++ src/Updates.js
@@ -0,0 +1,13 @@
+function Updates() {
+  return (
+    <div className="Updates">
+      <article>
+          <h1>Updates</h1>
+          updates from latest matches here, blog style
+        </article>
+    </div>
+  );
+}
+
+export default Updates;
+
