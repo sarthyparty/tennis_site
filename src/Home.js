c2acf531e4a12b8b6e96dd997f6eaccd2336fdca
Sarthak Agrawal <agraw189@umn.edu>
2022-06-07 09:50:10 -0500

    slideshow

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ function Home() {
           <h1>Home</h1>
           This is the home page will have continuous slideshow of pictures going
           and info about the team.
+          <br/><br/>
           <Slideshow/>
         </article>
     </div>
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,17 +3,20 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import image1 from "../images/tennis2.jpeg"
 import image2 from "../images/tennis3.jpeg"
+import "../styles/slideshow.css"
 
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-  <img src={image1} alt="" onDragStart={handleDragStart} role="presentation" width="fill"/>,
-  <img src={image2} alt="" onDragStart={handleDragStart} role="presentation" />,
+  <img src={image1} alt="" onDragStart={handleDragStart} role="presentation" width="90%" height="50%"/>,
+  <img src={image2} alt="" onDragStart={handleDragStart} role="presentation" width="90%" height="50%"/>,
   <img src="../../public/tennis.jpeg" alt="" onDragStart={handleDragStart} role="presentation" />,
 ];
 
 export const Slideshow = () => {
   return (
+    <div className='slideshow'>
     <AliceCarousel mouseTracking items={items} />
+    </div>
   );
 }
