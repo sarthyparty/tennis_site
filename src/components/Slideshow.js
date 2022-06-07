9a814c24a0d615135788e64bd88077d43d333803
Sarthak Agrawal <agraw189@umn.edu>
2022-06-06 23:12:11 -0500

    slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,25 +1,19 @@
-import SimpleImageSlider from "react-simple-image-slider";
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import image1 from "../images/tennis2.jpeg"
+import image2 from "../images/tennis3.jpeg"
 
-const images = [
-  { url: "../../public/logo192.png" },
-  { url: "../../public/logo192.png" },
-  { url: "../../public/logo192.png" },
-  { url: "../../public/logo192.png" },
-  { url: "../../public/logo192.png" },
-  { url: "../../public/logo192.png" },
-  { url: "../../public/logo192.png" },
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img src={image1} alt="" onDragStart={handleDragStart} role="presentation" width="fill"/>,
+  <img src={image2} alt="" onDragStart={handleDragStart} role="presentation" />,
+  <img src="../../public/tennis.jpeg" alt="" onDragStart={handleDragStart} role="presentation" />,
 ];
 
 export const Slideshow = () => {
   return (
-    <div>
-      <SimpleImageSlider
-        width={896}
-        height={504}
-        images={images}
-        showBullets={true}
-        showNavs={true}
-      />
-    </div>
+    <AliceCarousel mouseTracking items={items} />
   );
 }
