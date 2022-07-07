481939c82cc96a91c533a615ab2b3829e571514e
sarthyparty <sarthavader@gmail.com>
2022-07-06 22:40:52 -0500

    added links page and subscribe banner

---
+++ src/content/LinksPage.js
@@ -0,0 +1,10 @@
+export const links = [
+    {
+        name: "Calendar",
+        link: "https://srv3-advancedview.rschooltoday.com/public/conference/calendarreport/G5genie/23/G5button/13/school_id/3/ffpage/advanced/G5MID/052081071109056048054057085057079121108097120099075117068098100122077087043085075047089090098105052097056050047110098097078120075100073120098116115053099120067081082053066083103/preview/no/vw_activity/879/category/0/category_sel/12/vw_conference_events/1/vw_non_conference_events/1/vw_schoolonly/1/vw_homeonly/1/vw_awayonly/1/vw_gender/3/vw_type/0/vw_level/0/vw_opponent/0/vw_location/0/opt_show_bus_times/1/opt_show_changes_cancellations/1/vw_period/date-range/vw_month_from/06/vw_day_from/28/vw_year_from/2022/vw_month_to/12/vw_day_to/28/vw_year_to/2022/opt_show_location/1/opt_show_comments/1/multipleSchools/1"
+    },
+    {
+        name: "MyUTR",
+        link: "https://www.universaltennis.com/"
+    },
+]
